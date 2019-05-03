9d4e22f9ad8791ed75c94cfcc125aaae2a866a20
Cássio Marcos Goulart <cassiomgoulart@gmail.com>
2019-05-03 20:23:56 -0300

    Project Commit
    Project files

---
+++ Stack Views Layout/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Stack Views Layout
+//
+//  Created by Cássio Marcos Goulart on 12/04/19.
+//  Copyright © 2019 CMG Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // TODO: Create Tags for each button
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+    }
+    
+}
+
